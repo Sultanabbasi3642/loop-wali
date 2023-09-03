5cbe3c17be8a9c6fae12c9a112616b18b8809f59
sultan abbasi <ameersultanabbasi1998@gmail.com>
2023-09-03 12:14:35 -0700

    loop

---
+++ type.ts
@@ -0,0 +1,32 @@
+// let mybag= [5000]
+// // let mybag=[5337]
+
+// let x:unknown= 565
+// //console.log(x.length)
+ 
+// console.log( x as string)
+
+// console.log(typeof x);
+// let prompt=require("prompt-sync")()
+// let n:number=parseInt(prompt('Please enter the number:'));
+// let a:string=""
+// for(let i=1;i<=n;i++){ 
+//     a += "*";
+//     console.log(a);
+// }
+// export{}
+// let colors=["red","green","blue","yellow","pink"];
+// // for(let index in colors){
+// //     console.log(colors[index]);
+// // }
+// for(const index in colors){
+// console.log(colors[0]);
+// }
+var num:number = 5; 
+var factorial:number = 1; 
+
+while(num >=1) { 
+   factorial = factorial * num;
+   num -- ; 
+} 
+console.log("The factorial  is "+factorial);
